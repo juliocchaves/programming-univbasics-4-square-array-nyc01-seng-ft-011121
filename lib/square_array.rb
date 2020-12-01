d6ee518663de85efd2bfdc4ac2b0f77e94990187
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 03:32:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 numbers = [1, 2, 3]
 def square_array(numbers)
   new_numbers = []
-  numbers.each do |num|
-    new_numbers.push(num ** 2)
+  numbers.each do |index|
+    new_numbers.push(index ** 2)
   end
   new_numbers
 end
