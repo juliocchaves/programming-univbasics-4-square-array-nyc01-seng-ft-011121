9833bbde2b0d2cc81e371c9008be635067c4e8ca
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 03:40:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
   counter = 0
   new_numbers=[]
   while numbers[counter] do
-    new_numbers << numbers[counter]*numbers[counter]
+    new_numbers << numbers[counter] ** 2
     counter += 1
   end
   new_numbers
